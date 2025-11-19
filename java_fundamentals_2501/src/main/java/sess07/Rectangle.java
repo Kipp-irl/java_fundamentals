9c061b18105452e616fc3703e4b1e43fe463b603
Kipp-irl <victorkoech017@gmail.com>
2025-11-19 10:38:10 +0300

    Initial Commit

--- java_fundamentals_2501/src/main/java/sess07/Rectangle.java
+++ java_fundamentals_2501/src/main/java/sess07/Rectangle.java
@@ -1,46 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sess07;
 
 /**
- * The {@code Rectangle} class represents a 2-dimensional Rectangle with its length and width
- * It provides methods to compute its area and perimeter
+ * The {@code Rectangle} class represents a 2-dimensional Rectangle with its length and width.
+ * It provides methods to compute its area and perimeter.
  *
  * @author Victor
  */
-public class Rectangle {
-    protected int length;
-    protected int width;
+public class Rectangle extends Shape {
+
+    protected long length;  // The length of the rectangle
+    protected long width;   // The width of the rectangle
 
     /**
-     * @return the length
+     * Default constructor that initializes the rectangle with default values.
+     * Length and width are set to 0, and the name is set to "Rectangle".
      */
-    public int getLength() {
-        return length;
+    public Rectangle() {
+        super("Rectangle");  // Call to the superclass constructor to set the name
+        this.length = 0;
+        this.width = 0;
     }
 
     /**
-     * @param length the length to set
+     * Constructor that initializes the rectangle with the specified length and width.
+     * The name is set to the default value "Rectangle".
+     *
+     * @param length the length of the rectangle
+     * @param width the width of the rectangle
      */
-    public void setLength(int length) {
+    public Rectangle(long length, long width) {
+        super("Rectangle");  // Call to the superclass constructor to set the name
         this.length = length;
-    }
-
-    /**
-     * @return the width
-     */
-    public int getWidth() {
-        return width;
-    }
-
-    /**
-     * @param width the width to set
-     */
-    public void setWidth(int width) {
         this.width = width;
     }
-    
-    
+
+    /**
+     * Constructor that initializes the rectangle with the specified length, width, and name.
+     *
+     * @param length the length of the rectangle
+     * @param width the width of the rectangle
+     * @param name the name of the rectangle
+     */
+    public Rectangle(long length, long width, String name) {
+        super(name);  // Call to the superclass constructor to set the name
+        this.length = length;
+        this.width = width;
+    }
+
+    /**
+     * Gets the name of the rectangle.
+     *
+     * @return the name of the rectangle
+     */
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Calculates the area of the rectangle.
+     *
+     * @return the area of the rectangle
+     */
+    public long calcArea() {
+        return length * width;
+    }
+
+    /**
+     * Calculates the perimeter of the rectangle.
+     *
+     * @return the perimeter of the rectangle
+     */
+    public long calcPerimeter() {
+        return 2 * (length + width);
+    }
+
+    /**
+     * Returns a string representation of the rectangle, including its name, length, width,
+     * area, and perimeter.
+     *
+     * @return a string with details of the rectangle
+     */
+    @Override
+    public String toString() {
+        return this.getName() + " Details" + "\n" + "-".repeat(50) +
+               "\nLength: " + this.length +
+               "\nWidth: " + this.width +
+               "\nArea: " + this.calcArea() +
+               "\nPerimeter: " + this.calcPerimeter() +
+               "\n" + "-".repeat(50);
+    }
 }
--- java_fundamentals_2501/src/main/java/sess07/Shape.java
+++ java_fundamentals_2501/src/main/java/sess07/Shape.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sess07;
 
 /**
+ * The {@code Shape} class will be the super class of all shapes. It has a field name to
+ * hold the shape's name and an abstract method `getName()` that returns the name of the shape.
  *
  * @author Victor
  */
-public class Shape {
-    
+public abstract class Shape {
+    protected String name;  // The name of the shape
+
+    /**
+     * Constructor to initialize the name of the shape.
+     * 
+     * @param name the name of the shape
+     */
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Abstract method to get the name of the shape.
+     * 
+     * @return the name of the shape
+     */
+    public abstract String getName();
 }
